99615005f16240b0fd3b9bbb4d29c34337262464
Elena Grassi <grassi.e@gmail.com>
2019-03-06 17:24:35 +0100

    Splitted preliminary Seurat analyses to be more general

--- local/src/seurat.R
+++ local/src/seurat.R
@@ -19,11 +19,6 @@ name <- snakemake@params[["name"]]
 ### parameters that needs to be defined by hand after looking at the plots
 params <- snakemake@input[["pars"]]
 pars <- read.table(params, header=TRUE, sep="\t")
-clfile <- snakemake@input[["clfile"]]
-clusters <- read.csv(clfile) # "/home/egrassi/ba/dataset/cellranger/aggr/outs/analysis/clustering/kmeans_5_clusters/clusters.csv")
-barcodefile <- snakemake@input[["barcodes"]]
-#barcodes - samples
-osamples <- snakemake@params[["osamples"]]
 
 dir.create(outdir)
 library("Seurat")
@@ -72,52 +67,5 @@ plot_grid(p1, p2)
 graphics.off()
 
 
-# obtain sample from barcodes
-samples <- strsplit(colnames(srdata@data), '-', fixed=T)
-s <- sapply(samples, function(x) {x[2]})
-samples_id <- data.frame(id=s)
-translate <- data.frame(id=seq(1,5), sample=osamples)
-merged <- merge(samples_id, translate, by="id")
-rownames(merged) <- srdata@cell.names
-all(colnames(srdata@data)==srdata@cell.names)
-srdata <- AddMetaData(object = srdata, metadata =  merged)
-pdf("samples_plot.pdf")
-p1 <- TSNEPlot(srdata, do.return = T, pt.size = 0.5, group.by = "sample")
-p2 <- PCAPlot(object = srdata, dim.1 = 1, dim.2 = 2, group.by = "sample")
-plot_grid(p1, p2)
-graphics.off()
-
-
-pdf("tsne_samples.pdf"); 
-p1 <- TSNEPlot(srdata, do.return = T, pt.size = 0.5, group.by = "sample"); 
-plot(p1); 
-graphics.off()
-
-pdf("pca_samples.pdf"); 
-plot(p2); 
-graphics.off()
-
-# by hand I tried another resolution
-#srdata <- FindClusters(object = srdata, reduction.type = "pca", dims.use = 1:npc,  resolution = 0.6, print.output = 0, save.SNN = TRUE)
-#p1 <- TSNEPlot(srdata, do.return = T, pt.size = 0.5)
-#pdf("tsne_clusters.pdf"); plot(p1); dev.off()
-
-#pdf("pca_clusters.pdf")
-#p2 <- PCAPlot(object = srdata, dim.1 = 1, dim.2 = 2)
-#plot(p2)
-#graphics.off()
-
-#10x clustering is bad?
-
-
-## this part specific to aggr should be moved to another script TODO XXX needed when I'll need single sample analyses
-rownames(clusters) <- clusters$Barcode
-clusters$Barcode <- NULL
-srdata <- AddMetaData(object = srdata, metadata =  clusters)
-
-pdf("tsne_10x.pdf"); 
-p1 <- TSNEPlot(srdata, do.return = T, pt.size = 0.5, group.by = "Cluster"); 
-plot(p1); 
-graphics.off()
 setwd("..")
 save.image(outdata)
---
+++ local/src/seurat_check.R
@@ -0,0 +1,56 @@
+osamples <- snakemake@params[["osamples"]]
+infile <- snakemake@input[["data"]]
+clfile <- snakemake@input[["clfile"]]
+outdir <- snakemake@params[["outdir"]]
+clusters <- read.csv(clfile) # "/home/egrassi/ba/dataset/cellranger/aggr/outs/analysis/clustering/kmeans_5_clusters/clusters.csv")
+
+load(infile)
+library("Seurat")
+
+setwd(outdir)
+
+# obtain sample from barcodes
+samples <- strsplit(colnames(srdata@data), '-', fixed=T)
+s <- sapply(samples, function(x) {x[2]})
+samples_id <- data.frame(id=s)
+translate <- data.frame(id=seq(1,5), sample=osamples)
+merged <- merge(samples_id, translate, by="id")
+rownames(merged) <- srdata@cell.names
+all(colnames(srdata@data)==srdata@cell.names)
+srdata <- AddMetaData(object = srdata, metadata =  merged)
+pdf("samples_plot.pdf")
+p1 <- TSNEPlot(srdata, do.return = T, pt.size = 0.5, group.by = "sample")
+p2 <- PCAPlot(object = srdata, dim.1 = 1, dim.2 = 2, group.by = "sample")
+plot_grid(p1, p2)
+graphics.off()
+
+
+pdf("tsne_samples.pdf"); 
+p1 <- TSNEPlot(srdata, do.return = T, pt.size = 0.5, group.by = "sample"); 
+plot(p1); 
+graphics.off()
+
+pdf("pca_samples.pdf"); 
+plot(p2); 
+graphics.off()
+
+# by hand I tried another resolution
+#srdata <- FindClusters(object = srdata, reduction.type = "pca", dims.use = 1:npc,  resolution = 0.6, print.output = 0, save.SNN = TRUE)
+#p1 <- TSNEPlot(srdata, do.return = T, pt.size = 0.5)
+#pdf("tsne_clusters.pdf"); plot(p1); dev.off()
+
+#pdf("pca_clusters.pdf")
+#p2 <- PCAPlot(object = srdata, dim.1 = 1, dim.2 = 2)
+#plot(p2)
+#graphics.off()
+
+#10x clustering is bad?
+
+rownames(clusters) <- clusters$Barcode
+clusters$Barcode <- NULL
+srdata <- AddMetaData(object = srdata, metadata =  clusters)
+
+pdf("tsne_10x.pdf"); 
+p1 <- TSNEPlot(srdata, do.return = T, pt.size = 0.5, group.by = "Cluster"); 
+plot(p1); 
+graphics.off()
