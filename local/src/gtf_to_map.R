7f894ecb5ab5e9e4992145ee77b56eb90f8b3751
Elena Grassi <grassi.e@gmail.com>
2025-06-06 12:53:34 +0000

    WIP on gs-ENGS reverse remap for rCASC

--- local/src/gtf_to_map.R
+++ local/src/gtf_to_map.R
@@ -1,8 +1,27 @@
 library(rtracklayer)
 gtf_f <- snakemake@input[['gtf']]
 map_f <- snakemake@output[['map']]
+map_f_disamb <- snakemake@output[['map_disamb']]
 
 x <- data.frame(rtracklayer::import(gtf_f))
 dd <- unique(data.frame(gs=x$gene_name, ens=x$gene_id))
+save.image('pippo.Rdata')
+
+tto_disamb <- as.data.frame(table(dd$gs))
+to_disamb <- as.character(tto_disamb[tto_disamb$Freq > 1, 1] )
+
+ddd <- dd[!dd$gs %in% to_disamb,]
+dd_disamb <- dd[dd$gs %in% to_disamb,]
+
+choose_ensg <- function(gene, data) {
+	myd <- data[data$gs == gene, ]
+	myd[1,, drop=FALSE]
+}
+dd_disamb <- dd_disamb[order(dd_disamb$gs, dd_disamb$ens),]
+dd_disambiguato <- lapply(to_disamb, choose_ensg, dd_disamb)
+dd_disambiguato <- do.call(rbind, dd_disambiguato)
+
+dd_disambiguato <- rbind(dd_disambiguato, ddd)
 write.table(dd, file=map_f, sep="\t", quote=FALSE, row.names=FALSE)
+write.table(dd_disambiguato, file=map_f_disamb, sep="\t", quote=FALSE, row.names=FALSE)
 
